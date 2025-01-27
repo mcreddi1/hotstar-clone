510c0896d0e519dc0e5bbd0bd1c1a1aef67c7f17
mcreddi1 <morachinnapareddy1996@gmail.com>
2025-01-28 01:39:53 +0530

    chinna

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket = "krishna7730"
-    key    = "terraform/state/dev.tfstate"
-    region = "ap-south-1"
-    dynamodb_table = "krishna7730"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "krishna7730"
+#     key    = "terraform/state/dev.tfstate"
+#     region = "ap-south-1"
+#     dynamodb_table = "krishna7730"
+#   }
+# }
